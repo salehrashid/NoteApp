f604da34870efeb98fed55ef8263328539f64cbe
saleh rashid <saleh.rashid.b@gmailcom>
2022-02-28 15:06:49 +0700

    nav host controller added

--- app/src/main/java/com/app/notesapp/MainActivity.kt
+++ app/src/main/java/com/app/notesapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.app.notesapp
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.elevation = 0F
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?
+
+        if (navHostFragment != null){
+            val appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
+            setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
